a469586c39dff6616f4fdddb54144a004e401ffb
Mario Marić <m4ri000@gmail.com>
2013-07-06 02:36:25 +0200

    Using "sed" instead of "tr".
    Minor "bug" because of nature how "tr" linux command is doing its magic - "sed" linux command is more appropriate tool for the job of renaming words and not sets.

--- sbv2txt.sh
+++ sbv2txt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Requirements: grep, tr
+# Requirements: sed, grep, tr
 # Usage: sbv2txt input.sbv
 
 # Output file - change file extension from .sbv to .txt
-out=$(echo "$1" | tr 'sbv' 'txt')
+out=$(echo "$1" | sed 's/sbv/txt/')
 
 # Do the grep and tr magic :)
 grep -v -e "," "$1" | grep -v -e "^$" | tr '\n' ' ' > "$out"
