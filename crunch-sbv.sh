e7cf95e708c0b0cbef0350951a920deeee478e84
Mario Marić <git@mariomaric.net>
2013-09-30 21:34:55 +0200

    Replaced grep with sed, using only sed & tr commands now

--- crunch-sbv.sh
+++ crunch-sbv.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Requirements: sed, grep, tr
-# Usage: crunch-sbv input.sbv extension
+# Requirements: sed, tr
+# Usage: crunch-sbv captions.sbv extension
 
-# Output file - change file extension
 # If file extension is not provided use markdown i.e. .md
 if [ -z "$2" ]
   then
@@ -10,9 +9,11 @@ if [ -z "$2" ]
   else
     ext="$2"
 fi
-out=$(echo "$1" | sed 's/sbv/"$ext"/')
 
-# Do the grep and tr magic :)
-grep -v -e "," "$1" | grep -v -e "^$" | tr '\n' ' ' > "$out"
+# Prepare output file with default / provided extension
+out=$(echo "$1" | sed "s/sbv/$ext/")
+
+# Do the sed and tr magic :)
+sed -e "/,/d" -e "/^$/d" "$1" | tr '\n' ' ' > "$out"
 
 exit
