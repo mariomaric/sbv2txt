fe3bb29f27ddd0e7e9f0e3fd8aeacf74e49be1a1
Mario Marić <git@mariomaric.net>
2013-09-30 03:30:43 +0200

    Provide your own extension or use markdown as default

--- crunch-sbv.sh
+++ crunch-sbv.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Requirements: sed, grep, tr
-# Usage: sbv2txt input.sbv
+# Usage: crunch-sbv input.sbv extension
 
-# Output file - change file extension from .sbv to .txt
-out=$(echo "$1" | sed 's/sbv/txt/')
+# Output file - change file extension
+# If file extension is not provided use markdown i.e. .md
+if [ -z "$2" ]
+  then
+    ext="md"
+  else
+    ext="$2"
+fi
+out=$(echo "$1" | sed 's/sbv/"$ext"/')
 
 # Do the grep and tr magic :)
 grep -v -e "," "$1" | grep -v -e "^$" | tr '\n' ' ' > "$out"
